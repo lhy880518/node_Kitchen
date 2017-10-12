49baa4ba872ab66c9ee4fe06cd97c6bd3a29efab
lhy880518 <lhy880518@naver.com>
2017-10-13 01:28:46 +0900

    구조 조정 및 새 메뉴 추가
    상단 메뉴 페이지를 새폴더 추가하여 관리 효율 증대
    알고리즘 항목 추가

--- public/js/angular_kitchen.js
+++ public/js/angular_kitchen.js
@@ -8,31 +8,34 @@ app.config(function($routeProvider, $locationProvider){
     controller:'MainController'
   })
   .when('/mongodb_detail',{
-    templateUrl: './public/views/content/mongodb_detail.html',
+    templateUrl: './public/views/content/material/mongodb_detail.html',
     controller:'MongoDetailController'
   })
   .when('/express_detail',{
-    templateUrl: './public/views/content/express_detail.html',
+    templateUrl: './public/views/content/material/express_detail.html',
     controller:'ExpressDetailController'
   })
   .when('/nodejs_detail',{
-    templateUrl: './public/views/content/nodejs_detail.html',
+    templateUrl: './public/views/content/material/nodejs_detail.html',
     controller:'NodejsDetailController'
   })
   .when('/angular-detail',{
-    templateUrl: './public/views/content/angular_detail.html',
+    templateUrl: './public/views/content/material/angular_detail.html',
     controller:'AngularDetailController'
   })
+  .when('/algolithm_ceasar',{
+    templateUrl: './public/views/content/algolithm/algolithm_ceasar.html'
+  })
   .when('/board_list',{
-    templateUrl: './public/views/content/board_list.html',
+    templateUrl: './public/views/content/board/board_list.html',
     controller:'BoardController'
   })
   .when('/board_mod/:_id',{
-    templateUrl: './public/views/content/board_mod.html',
+    templateUrl: './public/views/content/board/board_mod.html',
     controller:'ModController'
   })
   .when('/board_write',{
-    templateUrl: './public/views/content/board_write.html',
+    templateUrl: './public/views/content/board/board_write.html',
     controller:'WriteController'
   })
   .otherwise({
@@ -66,6 +69,10 @@ app.controller('AngularDetailController', function($scope, $http){
   $http.get('/angular_detail').then(function(data){
   });
 });
+app.controller('AlgolithmCeasarController', function($scope, $http){
+
+});
+
 
 app.controller('BoardController', function($scope, $http){
   $http.get('/board_list').then(function(data){
